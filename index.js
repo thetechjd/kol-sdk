724f499ee4850a319682c9572818ea6f56fd8805
rakesh9494 <ummadirakesh9494@gmail.com>
2025-05-29 15:17:26 +0530

    CRYP-34 sdk created

---
+++ index.js
@@ -0,0 +1,58 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+class CadetSDK {
+    constructor(apiKey, secretKey, baseUrl = 'https://api.cryptocadet.app/api') {
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
+        this.baseUrl = baseUrl;
+        this.token = null; // Store the authentication token
+        if (!apiKey || !secretKey) {
+            throw new Error('Both apiKey and secretKey are required.');
+        }
+        this.client = axios_1.default.create({
+            baseURL: baseUrl,
+            headers: {
+                'X-Secret-Key': secretKey,
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+    /**
+    * Authenticate and retrieve a session token
+    */
+    async authenticate() {
+        var _a, _b;
+        try {
+            const response = await this.client.post('/admin/authenticate', {
+                publicKey: this.apiKey,
+                secretKey: this.secretKey,
+            });
+            // Update the correct token field
+            this.token = response.data.sessionToken;
+            // Dynamically update the Authorization header
+            this.client.defaults.headers.Authorization = `Bearer ${this.token}`;
+        }
+        catch (error) {
+            throw new Error(((_b = (_a = error.response) === null || _a === void 0 ? void 0 : _a.data) === null || _b === void 0 ? void 0 : _b.message) || error.message);
+        }
+    }
+
+
+    async getKolTweets() {
+        var _a, _b;
+        try {
+            const response = await this.client.get('/user/get-kol-tweets');
+            return response.data;
+        }
+        catch (error) {
+            throw new Error(((_b = (_a = error.response) === null || _a === void 0 ? void 0 : _a.data) === null || _b === void 0 ? void 0 : _b.message) || error.message);
+        }
+    }
+}
+
+exports.default = CadetSDK;
